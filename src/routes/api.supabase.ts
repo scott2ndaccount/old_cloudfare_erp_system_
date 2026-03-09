14997eefc92698da2824b6919878e382d5320a13
Scott <sschmi2@ilstu.edu>
2026-03-09 17:26:25 -0500

    Create api.supabase.ts

---
+++ src/routes/api.supabase.ts
@@ -0,0 +1,14 @@
+export async function GET({ context }) {
+  const response = await fetch(
+    `${context.env.SUPABASE_URL}/rest/v1/`,
+    {
+      headers: {
+        apikey: context.env.SUPABASE_KEY
+      }
+    }
+  )
+
+  const data = await response.json()
+
+  return Response.json(data)
+}
