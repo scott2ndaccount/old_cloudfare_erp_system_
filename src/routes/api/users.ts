5e4d92ec2781dbda54481739cc8efe3be8fc563f
Scott <sschmi2@ilstu.edu>
2026-03-11 13:15:41 -0500

    Update users.ts

--- src/routes/api/users.ts
+++ src/routes/api/users.ts
@@ -1,6 +1,6 @@
 
 import { createFileRoute } from "@tanstack/react-router"
-import { getSupabaseClient } from "../../lib/supabase"
+import { getSupabaseClient } from './supabase'
 
 export const Route = createFileRoute("/api/users")({
   server: {
